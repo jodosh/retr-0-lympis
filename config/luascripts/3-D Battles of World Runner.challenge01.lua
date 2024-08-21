2f74f402b28f3d7373ed5943ec784ef2356f101c
jodosh <jodosh@users.noreply.github.com>
2024-08-21 15:30:52 -0500

    Update 3-D Battles of World Runner.challenge01.lua

--- config/luascripts/3-D Battles of World Runner.challenge01.lua
+++ config/luascripts/3-D Battles of World Runner.challenge01.lua
@@ -1,49 +1,71 @@
+local coreFcn = require("core_functions")
+
+local final_time = nil
+local completion_time = nil
+
 -- Initialize variables
-local startFrame = nil
-local bossBeat = false
-local challengeName = "Beat Boss 1"
+local saveStatePath = "../fcs/3-D Battles of World Runner.challenge01.fc0"
+local start_frame = nil
+local enddisplaytime = 0
+local countdown_frames = 180 -- Number of frames for the countdown (180 = 2 seconds)
+local message_countdown_timer = countdown_frames
 
 -- Function to check if Mario has picked up a mushroom
-function checkMushroom()
-    local powerupStatus = memory.readbyte(0x00EE) -- Address for world
-    if powerupStatus == 1 and not bossBeat then
-        bossBeat = true
-        local endFrame = emu.framecount()
-        local timeTaken = endFrame - startFrame
-
-        -- Display the result on the screen
-        gui.text(10, 10, challengeName)
-        gui.text(10, 30, "Beat Boss 1: " .. timeTaken .. " frames")
-
-        -- Update the screen
-        emu.frameadvance()
-
-        local file = io.open("3Dworld.txt", "a")
-
-        -- Pause the emulation indefinitely
-        while true do
-            gui.text(10, 10, challengeName)
-            gui.text(10, 30, "Beat Boss 1: " .. timeTaken .. " frames")
-            emu.frameadvance()
-
-            -- Wait for 5 seconds, then quit
-            if emu.framecount() - endFrame >= 300 then
-                -- Write the timeTaken to the file
-                file:write(timeTaken .. "\n")
-                emu.exit()
-            end
-        end
+function checkVictory()
+    local levelStatus = memory.readbyte(0x00EE) -- Address for world
+    if levelStatus == 1 then
+        final_time = emu.framecount() - start_frame
+        
+        local medal_message = coreFcn.get_medal_message(final_time)
+        coreFcn.display_centered_message({
+            "Congrats! You completed challenge",
+            "in " .. coreFcn.frames_to_time(final_time) .. " seconds",
+            "",  -- Space between time and medal message
+            medal_message[1],  -- First line of medal message
+            medal_message[2]   -- Second line of medal message
+        })
+        emu.pause()
     end
+end
+
+local function reset_challenge()
+    --todo
+    enddisplaytime = 0
+    message_countdown_timer = countdown_frames
+    final_time = nil
+    completion_time = nil  -- Reset completion time
+    coreFcn.set_medal_times(20, 10, 5)
 end
 
 -- Main loop
 while true do
-    if startFrame == nil then
-        startFrame = emu.framecount()
+    
+    coreFcn.display_title({"3D World Runner Boss Challenge"})
+
+    -- Display Instructions and Countdown (-60 so it keeps the message for an extra second)
+    if message_countdown_timer >= -120 then
+		-- Do the Countdown
+		if message_countdown_timer > 0 then 
+			coreFcn.message_and_countdown({"Beat the boss!"}, message_countdown_timer)
+		elseif message_countdown_timer == 0 then
+			local state = savestate.create(saveStatePath)
+			savestate.load(state)
+			start_frame = emu.framecount()
+		else
+			coreFcn.display_centered_message({"Beat the boss!"})
+		end
+	
+		message_countdown_timer = message_countdown_timer - 1
     end
 
-    if not mushroomPicked then
-        checkMushroom()
+    if start_frame == nil then
+        start_frame = emu.framecount()
+    end
+
+    checkVictory()
+
+    if coreFcn.restart_or_abort() then
+        reset_challenge()
     end
 
     emu.frameadvance() -- Advance to the next frame
