e449d478bad047b850681eac4900c79ebaa053ab
jodosh <jodosh@users.noreply.github.com>
2024-08-21 13:37:32 -0500

    Update ConsoleApp.cs

--- ConsoleApp.cs
+++ ConsoleApp.cs
@@ -18,60 +18,10 @@ namespace Retr0lympis
         public ConsoleApp()
         {
             InitializeGames();
-            //InitializeChallenges();
         }
 
         private void InitializeGames()
         {
-            /*
-            //SuperMario Bros 1
-            Game Smb1 = new("Super Mario Bros.", "Super Mario Bros.nes");
-            Smb1.AddChallenge("Get the first mushroom", "SMBChallenge1.fcs", "SMB.challenge01.lua");
-            games.Add(Smb1);
-
-            //The 3-D Battles of World Runner
-            Game WorldRunner = new("The 3-D Battles of World Runner", "3-D Battles of World Runner, The.nes");
-            WorldRunner.AddChallenge("Beat Boss 1", "3-D Battles of World Runner.challenge01.fc0", "3-D Battles of World Runner.challenge01.lua");
-            games.Add(WorldRunner);
-
-
-            //Zelda 2 - The Adventure of Link
-            Game Zelda2 = new("Zelda 2 - The Adventure of Link", "Zelda 2 - The Adventure of Link.nes");
-            Zelda2.AddChallenge("Magic in the Dark", "Z2Challenge1.fcs", "Zelda2.challenge01.lua");
-            Zelda2.AddChallenge("Kill Horsehead!", "Z2Challenge2.fcs", "Zelda2.challenge02.lua");
-            Zelda2.AddChallenge("Get that Trophy!", "Z2TrophyChallenge.fcs", "Zelda2.challenge03.lua");
-            Zelda2.AddChallenge("Get Life 2", "Z2Challenge4.fcs", "Zelda2.challenge04.lua");
-            games.Add(Zelda2);
-
-            //TMNT
-            Game Tmnt = new("Teenage Mutant Ninja Turtles",  "TMNT.nes");
-            Tmnt.AddChallenge("Clear the underwater level", "TMNT.challenge01.fcs", "TMNT.challenge01.lua");
-            Tmnt.AddChallenge("Beat Rocksteady with Raph", "TMNT.challenge02.fcs", "TMNT.challenge02.lua");
-            games.Add(Tmnt);
-
-            //CV1
-            Game Cv1 = new("Castlevania", "Castlevania.nes");
-            Cv1.AddChallenge("Kill bat.. knife only", "CV1KillTheBat.fcs", "CV1BatKnifeChallenge.lua");
-            Cv1.AddChallenge("Get 10 hearts!", "CV1Get10Hearts.fcs", "CV110HeartChallenge.lua");
-            Cv1.AddChallenge("Escape the Castle!", "CV1CastleEscape.fcs", "CVCastleEscapeChallenge.lua");
-            Cv1.AddChallenge("Escape the Castle! PART II", "CV1CastleEscapePart2.fcs", "CVCastleEscapePart2Challenge.lua");
-            Cv1.AddChallenge("Knight Alley - Find Death!", "CV1KnightAlley.fcs", "CV1KnightAlleyChallenge.lua");
-            Cv1.AddChallenge("Cheat Death - Survival Challenge", "CV1CheatDeathChallenge.fcs", "CV1CheatDeathChallenge.lua");
-            games.Add(Cv1);
-
-            //Kid Icarus
-            Game KidIcarus = new("Kid Icarus", "Kid Icarus.nes");
-            KidIcarus.AddChallenge("Finish 1-1", "KidIcarus11challenge.fcs", "KidIcarus11Challenge.lua");            
-            games.Add(KidIcarus);
-
-            //Kid Icarus
-            Game SuperDodgeBall = new("Super Dodge Ball", "Super Dodge Ball.nes");
-            SuperDodgeBall.AddChallenge("Tom must DIE!", "SuperDodgeTomMustDieChallenge.fcs", "SuperDodgeBallTomMustDieChallenge.lua");
-            games.Add(SuperDodgeBall);
-
-            games.Sort((x,y) => string.Compare(x.Name,y.Name));
-            */
-
             // Read the JSON from the file
             var options = new JsonSerializerOptions
             {
