c3df8794e776804add3b9a4c20952d6cfd15af31
mattd1980 <58088480+mattd1980@users.noreply.github.com>
2024-08-27 20:05:19 -0400

    added support for an audio player (#10)
    * added support for an audio player
    
    updated the yaml files
    
    * fixing build failure
    
    * fixing arm build
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    ---------
    
    Co-authored-by: jodosh <jodosh@users.noreply.github.com>

---
+++ AudioPlayer.cs
@@ -0,0 +1,49 @@
+﻿using NetCoreAudio;
+using System;
+using System.Threading.Tasks;
+
+namespace Retr0lympis
+{
+    public class AudioPlayer
+    {
+        private Player _player;
+
+        public AudioPlayer()
+        {
+            _player = new Player();
+        }
+
+        public async Task PlaySoundAsync(string filePath)
+        {
+            try
+            {
+                if (!_player.Playing)
+                {
+                    await _player.Play(filePath);
+                    Console.WriteLine($"Playing sound: {filePath}");
+                }
+                else
+                {
+                    Console.WriteLine("A sound is already playing. Please wait.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error playing sound: {ex.Message}");
+            }
+        }
+
+        public async Task StopSoundAsync()
+        {
+            try
+            {
+                await _player.Stop();
+                Console.WriteLine("Sound stopped.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error stopping sound: {ex.Message}");
+            }
+        }
+    }
+}
